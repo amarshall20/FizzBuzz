bf106bd609de8f9cb8f12a9e68f260e2cf0b0961
amarshall20 <33075034+amarshall20@users.noreply.github.com>
2018-01-17 14:41:53 -0700

    UpandDown

--- RecursiveFizzBuzz/Program.cs
+++ RecursiveFizzBuzz/Program.cs
@@ -10,7 +10,8 @@ namespace RecursiveFizzBuzz
     {
         static void Main(string[] args)
         {
-            FizzBuzz(100);
+            FizzBuzz(15);
+            UpAndDownFizzBuzz(0,15);
             Console.ReadLine();
         }
         static void FizzBuzz(int n)
@@ -44,6 +45,55 @@ namespace RecursiveFizzBuzz
                     }
                 }
                 FizzBuzz(n - 1);
+            }
+        }
+        static void UpAndDownFizzBuzz(int begin, int end)
+        {
+            if (begin <= end)
+            {
+                if (begin == 0)
+                    Console.WriteLine(begin);
+                else
+                {
+                    if (begin % 3 == 0)
+                    {
+                        if (begin % 5 == 0)
+                            Console.WriteLine("FizzBuzz");
+                        else
+                            Console.WriteLine("Fizz");
+                    }
+                    else
+                    {
+                        if (begin % 5 == 0)
+                            Console.WriteLine("Buzz");
+                        else
+                            Console.WriteLine(begin);
+                    }
+                }
+                UpAndDownFizzBuzz(begin+1, end);
+            }
+            else
+            {
+                if (end == 0)
+                    Console.WriteLine(end);
+                else
+                {
+                    if (end % 3 == 0)
+                    {
+                        if (end % 5 == 0)
+                            Console.WriteLine("FizzBuzz");
+                        else
+                            Console.WriteLine("Fizz");
+                    }
+                    else
+                    {
+                        if (end % 5 == 0)
+                            Console.WriteLine("Buzz");
+                        else
+                            Console.WriteLine(end);
+                    }
+                    UpAndDownFizzBuzz(begin, end-1);
+                }
             }
         }
     }
