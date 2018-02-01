0759f4210a23ea60fdffbf2ff96a3b6f8b7ff817
amarshall20 <33075034+amarshall20@users.noreply.github.com>
2018-02-01 16:18:39 -0700

    Cleanup

--- RecursiveFizzBuzz/Program.cs
+++ RecursiveFizzBuzz/Program.cs
@@ -10,17 +10,43 @@ namespace RecursiveFizzBuzz
     {
         static void Main(string[] args)
         {
+            LoopingFizzBuzz(15);
             FizzBuzz(15);
-            UpAndDownFizzBuzz(0,15);
+            UpAndDownFizzBuzz(0, 15);
             Console.ReadLine();
+        }
+        static void LoopingFizzBuzz(int n)
+        {
+            while (n > 0)
+            {
+                if (n % 3 == 0)
+                {
+                    if (n % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else
+                {
+                    if (n % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(n);
+                    }
+                }
+                n--;
+            }
         }
         static void FizzBuzz(int n)
         {
-            if (n == 0)
-            {
-                Console.WriteLine(n);
-            }
-            else
+            if (n != 0)
             {
                 if (n % 3 == 0)
                 {
@@ -51,9 +77,7 @@ namespace RecursiveFizzBuzz
         {
             if (begin <= end)
             {
-                if (begin == 0)
-                    Console.WriteLine(begin);
-                else
+                if (begin != 0)
                 {
                     if (begin % 3 == 0)
                     {
@@ -70,13 +94,11 @@ namespace RecursiveFizzBuzz
                             Console.WriteLine(begin);
                     }
                 }
-                UpAndDownFizzBuzz(begin+1, end);
+                UpAndDownFizzBuzz(begin + 1, end);
             }
             else
             {
-                if (end == 0)
-                    Console.WriteLine(end);
-                else
+                if (end != 0)
                 {
                     if (end % 3 == 0)
                     {
@@ -92,7 +114,7 @@ namespace RecursiveFizzBuzz
                         else
                             Console.WriteLine(end);
                     }
-                    UpAndDownFizzBuzz(begin, end-1);
+                    UpAndDownFizzBuzz(begin, end - 1);
                 }
             }
         }
