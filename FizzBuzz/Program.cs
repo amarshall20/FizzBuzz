d4bd9675adea3fd7b4dafa372d4966863fbffa3b
amarshall20 <33075034+amarshall20@users.noreply.github.com>
2018-02-14 11:32:40 -0700

    Refactoring

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,138 +10,42 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(LoopingFizzBuzz(15));
-            //need to provide string to concatenate it recursively
-            string fizzBuzz = "";
-            Console.WriteLine(RecursiveFizzBuzz(fizzBuzz, 15));
-            //need to provide beginning and end to go up and down recursively
-            Console.WriteLine(UpAndDownFizzBuzz(fizzBuzz, 1, 15));
+            Console.WriteLine(Looping(15));
+            Console.WriteLine(DownUpRecursive(15));
             Console.ReadLine();
         }
-        static string LoopingFizzBuzz(int n)
+        static string Looping(int n)
         {
             string fizzBuzz = "";
             while (n > 0)
             {
-                if (n % 3 == 0)
-                {
-                    if (n % 5 == 0)
-                        fizzBuzz += "FizzBuzz\n";
-                    else
-                        fizzBuzz += "Fizz\n";
-                }
-                else
-                {
-                    if (n % 5 == 0)
-                        fizzBuzz += "Buzz\n";
-                    else
-                        fizzBuzz += n + "\n";
-                }
+                fizzBuzz += FizzBuzz(n);
                 n--;
             }
             return fizzBuzz.TrimEnd('\n');
         }
-        static string RecursiveFizzBuzz(string fizzBuzz, int n)
+        static string DownUpRecursive(int n)
         {
-            if (n > 0)
+            string fizzBuzz = "";
+            if (n > 1)
             {
-                if (n % 3 == 0)
-                {
-                    if (n % 5 == 0)
-                    {
-                        fizzBuzz += "FizzBuzz\n";
-                        return RecursiveFizzBuzz(fizzBuzz, n-1);
-                    }
-                    else
-                    {
-                        fizzBuzz += "Fizz\n";
-                        return RecursiveFizzBuzz(fizzBuzz, n-1);
-                    }
-                }
-                else
-                {
-                    if (n % 5 == 0)
-                    {
-                        fizzBuzz += "Buzz\n";
-                        return RecursiveFizzBuzz(fizzBuzz, n-1);
-                    }
-
-                    else
-                    {
-                        fizzBuzz += n + "\n";
-                        return RecursiveFizzBuzz(fizzBuzz, n-1);
-                    }
-                }
-            }else
-            {
-                return fizzBuzz.TrimEnd('\n');
+                fizzBuzz += FizzBuzz(n) + DownUpRecursive(n - 1);
+                return fizzBuzz + FizzBuzz(n);
             }
+            return n.ToString() + "\n";
         }
-        static string UpAndDownFizzBuzz(string fizzBuzz, int begin, int end)
+        static string FizzBuzz(int n)
         {
-            if (begin <= end)
-            {
-                if (begin % 3 == 0)
-                {
-                    if (begin % 5 == 0)
-                    {
-                        fizzBuzz += "FizzBuzz\n";
-                        return UpAndDownFizzBuzz(fizzBuzz, begin + 1, end);
-                    }
-                    else
-                    {
-                        fizzBuzz += "Fizz\n";
-                        return UpAndDownFizzBuzz(fizzBuzz, begin + 1, end);
-                    }
-                }
-                else
-                {
-                    if (begin % 5 == 0)
-                    {
-                        fizzBuzz += "Buzz\n";
-                        return UpAndDownFizzBuzz(fizzBuzz, begin + 1, end);
-                    }
-                    else{
-                        fizzBuzz += begin + "\n";
-                        return UpAndDownFizzBuzz(fizzBuzz, begin + 1, end);
-                    }
-                }
-            }
+            string fizzBuzz = "";
+            if (n % 15 == 0)
+                fizzBuzz += "FizzBuzz\n";
+            else if (n % 5 == 0)
+                fizzBuzz += "Buzz\n";
+            else if (n % 3 == 0)
+                fizzBuzz += "Fizz\n";
             else
-            {
-                if (end > 0)
-                {
-                    if (end % 3 == 0)
-                    {
-                        if (end % 5 == 0)
-                        {
-                            fizzBuzz += "FizzBuzz\n";
-                            return UpAndDownFizzBuzz(fizzBuzz, begin, end - 1);
-                        }
-                        else
-                        {
-                            fizzBuzz += "Fizz\n";
-                            return UpAndDownFizzBuzz(fizzBuzz, begin, end - 1);
-                        }
-                    }
-                    else
-                    {
-                        if (end % 5 == 0)
-                        {
-                            fizzBuzz += "Buzz\n";
-                            return UpAndDownFizzBuzz(fizzBuzz, begin, end - 1);
-                        }
-                        else
-                        {
-                            fizzBuzz += end + "\n";
-                            return UpAndDownFizzBuzz(fizzBuzz, begin, end - 1);
-                        }
-                    }
-                }else
-                {
-                    return fizzBuzz.TrimEnd('\n');
-                }
-            }
+                fizzBuzz += n + "\n";
+            return fizzBuzz;
         }
     }
 }
